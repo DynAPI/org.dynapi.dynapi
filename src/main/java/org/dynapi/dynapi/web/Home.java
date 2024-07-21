3d116e01dcf32b3a9de5a48f45247603e0b1ee5c
playerg9 <g9yout@gmail.com>
2024-07-21 18:02:52 +0200

    added /redoc endpoint

--- src/main/java/org/dynapi/dynapi/web/Home.java
+++ src/main/java/org/dynapi/dynapi/web/Home.java
@@ -1,10 +1,15 @@
 package org.dynapi.dynapi.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.InputStream;
 
 @Slf4j
 @RestController
@@ -13,5 +18,15 @@ public class Home {
     @GetMapping("/")
     public ResponseEntity<String> index() {
         return ResponseEntity.ok("Hello World");
+    }
+
+    @GetMapping(value = "/redoc", produces = "text/html")
+    public ResponseEntity<?> redoc() {
+        InputStream stream = getClass().getResourceAsStream("/static/html/redoc.html");
+        if (stream == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to load redoc.html");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new InputStreamResource(stream));
     }
 }
