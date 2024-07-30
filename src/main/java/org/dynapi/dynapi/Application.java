8ff25f0680c60382790ce9bf29d42fcee2086ac3
playerg9 <g9yout@gmail.com>
2024-07-30 19:04:23 +0200

    added custom configuration system

--- src/main/java/org/dynapi/dynapi/Application.java
+++ src/main/java/org/dynapi/dynapi/Application.java
@@ -1,15 +1,25 @@
 package org.dynapi.dynapi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.dynapi.dynapi.core.config.DynAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@Configuration
 @SpringBootApplication
 public class Application {
+    public static DynAPIConfiguration configuration = DynAPIConfiguration.load();
 
     public static void main(String[] args) {
+        SystemPropertyUpdater.updateProperties(configuration);
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public DynAPIConfiguration dynAPIConfiguration() {
+        return configuration;
+    }
 }
