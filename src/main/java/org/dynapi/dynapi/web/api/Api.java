55fe160700310406036ad9f48b58e12056632d09
playerg9 <g9yout@gmail.com>
2024-08-05 19:17:01 +0200

    added/updated rest-controller

---
+++ src/main/java/org/dynapi/dynapi/web/api/Api.java
@@ -0,0 +1,17 @@
+package org.dynapi.dynapi.web.api;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * query endpoint for the data
+ */
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class Api {
+
+}
