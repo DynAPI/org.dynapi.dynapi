da6e67f79860ae8103769315527c49e0ddad431d
playerg9 <g9yout@gmail.com>
2024-08-13 21:23:08 +0200

    improved MetaQueries for PostgreSQL

--- src/main/java/org/dynapi/dynapi/core/db/meta/impl/PostgreSQLMetaQueries.java
+++ src/main/java/org/dynapi/dynapi/core/db/meta/impl/PostgreSQLMetaQueries.java
@@ -8,34 +8,55 @@ import org.dynapi.squirtle.core.terms.criterion.Field;
 
 public class PostgreSQLMetaQueries implements MetaQueryGenerator {
     @Override
-    public String listSchemasQuery() {
+    public String listSchemas() {
+        Table pgNamespace = new Schema("pg_catalog").table("pg_namespace");
+        Field schemaName = pgNamespace.field("nspname", "schemaname");
         return PostgreSQLQuery
-            .from(new Schema("pg_catalog").getTable("pg_namespace"))
-            .select(new Field("schemaname", "nspname"))
+            .from(pgNamespace)
+            .select(schemaName)
+            .where(schemaName.not_like("pg_%"))
             .getSql();
     }
 
     @Override
-    public String listTablesQuery() {
+    public String listTables() {
+        Table pgTables = new Schema("pg_catalog").table("pg_tables");
+        Field schemaName = pgTables.field("schemaname");
+        Field tableName = pgTables.field("tablename");
         return PostgreSQLQuery
-                .from(new Schema("pg_catalog").getTable("pg_tables"))
-                .select("schemaname", "tablename")
+                .from(pgTables)
+                .select(schemaName, tableName)
+                .where(schemaName.not_like("pg_%"))
+                .where(tableName.not_like("pg_%"))
                 .getSql();
     }
 
     @Override
-    public String listTableOfSchemaQuery(String schemaName) {
-        Table table = new Schema("pg_catalog").getTable("pg_tables");
-        Field schemaNameField = table.field("schemaname");
+    public String listTableOfSchema(String ofSchemaName) {
+        Table pgTables = new Schema("pg_catalog").table("pg_tables");
+        Field schemaName = pgTables.field("schemaname");
+        Field tableName = pgTables.field("tablename");
         return PostgreSQLQuery
-                .from(table)
-                .select(schemaNameField, "tablename")
-                .where(schemaNameField.eq(schemaName))
+                .from(pgTables)
+                .select(schemaName, tableName)
+                .where(schemaName.not_like("pg_%"))
+                .where(tableName.not_like("pg_%"))
+                .where(schemaName.eq(ofSchemaName))
                 .getSql();
     }
 
     @Override
-    public String listColumnsOfTableQuery(String schemaName, String tableName) {
-        throw new RuntimeException("Not implemented yet");
+    public String listColumnsOfTable(String schema, String table) {
+        Table columns = new Schema("information_schema").table("columns");
+        Field tableSchema = columns.field("table_schema");
+        Field tableName = columns.field("table_name");
+        return PostgreSQLQuery
+                .from(columns)
+                .select(
+                        columns.field("column_name", "column_name"),
+                        columns.field("udt_name", "type")
+                )
+                .where(tableSchema.eq(schema).and(tableName.eq(table)))
+                .getSql();
     }
 }
