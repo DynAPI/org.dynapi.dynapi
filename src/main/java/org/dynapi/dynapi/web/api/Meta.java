452b38f7a918b48b92ec7bbdbe99470447022fa4
playerg9 <g9yout@gmail.com>
2024-08-24 18:42:14 +0200

    added OpenApi-Specification for /meta endpoints

--- src/main/java/org/dynapi/dynapi/web/api/Meta.java
+++ src/main/java/org/dynapi/dynapi/web/api/Meta.java
@@ -1,15 +1,19 @@
 package org.dynapi.dynapi.web.api;
 
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dynapi.dynapi.core.db.meta.MetaQueryGenerator;
+import org.dynapi.dynapi.core.openapi.OpenAPIManager;
+import org.dynapi.dynapi.core.openapi.OpenAPIProvider;
+import org.dynapi.openapispec.OpenApiSpecBuilder;
+import org.dynapi.openapispec.core.*;
+import org.dynapi.openapispec.core.objects.*;
+import org.dynapi.openapispec.core.types.*;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -22,9 +26,14 @@ import java.util.Map;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/meta")
-public class Meta {
+public class Meta implements OpenAPIProvider {
     private JdbcTemplate database;
     private MetaQueryGenerator queries;
+
+    @PostConstruct
+    public void init() {
+        OpenAPIManager.addProvider(this);
+    }
 
     @GetMapping(value = "/schemas", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> schemas() {
@@ -57,5 +66,115 @@ public class Meta {
         log.info(sqlQuery);
         List<Map<String, Object>> data = database.queryForList(sqlQuery);
         return ResponseEntity.ok(data);
+    }
+
+    @Override
+    public void generateOpenAPISpecification(OpenApiSpecBuilder builder) {
+        builder.addTag(Tag.builder()
+                .name("meta")
+                .description("Endpoints related to query information about the database")
+                .build()
+        );
+
+        builder.addPath("/schemas", new PathBuilder()
+                .addMethod(OperationType.GET, new OperationBuilder()
+                        .addTag("meta")
+                        .summary("Lists available schemas")
+                        .description("Lists information about the available schemas")
+                        .addResponse(200, new ResponseBuilder()
+                                .description("Success")
+                                .addContent(new TArray()
+                                        .addType(new TString())
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build()
+        );
+
+        builder.addPath("/tables", new PathBuilder()
+                .addMethod(OperationType.GET, new OperationBuilder()
+                        .addTag("meta")
+                        .summary("Lists tables and their schema")
+                        .description("Lists information about the available tables")
+                        .addResponse(200, new ResponseBuilder()
+                                .description("Success")
+                                .addContent(new TArray()
+                                        .addType(new TObject()
+                                                .addProperty("schemaname", new TString())
+                                                .addProperty("tablename", new TString())
+                                        )
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build()
+        );
+
+        builder.addPath("/tables/{schema}", new PathBuilder()
+                .addCommonParameter(Parameter.builder()
+                        .in(Parameter.In.PATH)
+                        .required(true)
+                        .name("schema")
+                        .description("Name of the database schema")
+                        .schema(new TString())
+                        .build()
+                )
+                .addMethod(OperationType.GET, new OperationBuilder()
+                        .addTag("meta")
+                        .summary("Lists tables if a schema")
+                        .description("Lists information about the tables of a schema")
+                        .addResponse(200, new ResponseBuilder()
+                                .description("Success")
+                                .addContent(new TArray()
+                                        .addType(new TObject()
+                                                .addProperty("schemaname", new TString())
+                                                .addProperty("tablename", new TString())
+                                        )
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build()
+        );
+
+        builder.addPath("/columns/{schema}/{table}", new PathBuilder()
+                .addCommonParameter(Parameter.builder()
+                        .in(Parameter.In.PATH)
+                        .required(true)
+                        .name("schema")
+                        .description("Name of the database schema")
+                        .schema(new TString())
+                        .build()
+                )
+                .addCommonParameter(Parameter.builder()
+                        .in(Parameter.In.PATH)
+                        .required(true)
+                        .name("table")
+                        .description("Name of the database table")
+                        .schema(new TString())
+                        .build()
+                )
+                .addMethod(OperationType.GET, new OperationBuilder()
+                        .addTag("meta")
+                        .summary("Lists columns of a table")
+                        .description("Lists information about the columns of a table")
+                        .addResponse(200, new ResponseBuilder()
+                                .description("Success")
+                                .addContent(new TArray()
+                                        .addType(new TObject()
+                                                .addProperty("column_name", new TString())
+                                                .addProperty("type", new TString())
+                                        )
+                                )
+                                .build()
+                        )
+                        .build()
+                )
+                .build()
+        );
     }
 }
