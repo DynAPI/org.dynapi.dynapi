6ddb5209dd943c521007d51a0af0f892adc34396
playerg9 <g9yout@gmail.com>
2024-08-24 21:23:35 +0200

    improved SQLiteMetaQueries#listTables

--- src/main/java/org/dynapi/dynapi/core/db/meta/impl/SQLiteMetaQueries.java
+++ src/main/java/org/dynapi/dynapi/core/db/meta/impl/SQLiteMetaQueries.java
@@ -3,25 +3,34 @@ package org.dynapi.dynapi.core.db.meta.impl;
 import org.dynapi.dynapi.core.db.meta.MetaQueryGenerator;
 import org.dynapi.squirtle.core.dialects.sqlite.SQLiteQuery;
 import org.dynapi.squirtle.core.queries.Schema;
+import org.dynapi.squirtle.core.queries.Selectable;
 import org.dynapi.squirtle.core.queries.Table;
 import org.dynapi.squirtle.core.terms.criterion.Field;
 import org.dynapi.squirtle.core.terms.functions.TableValuedFunction;
 import org.dynapi.squirtle.core.terms.values.ValueWrapper;
 
-import java.util.List;
-
 public class SQLiteMetaQueries implements MetaQueryGenerator {
 
     @Override
     public String listSchemas() {
+        // even if the pragma is called "database_list" it basically the schemas
+        Selectable pragma = new TableValuedFunction("pragma_database_list");
         return new SQLiteQuery()
-                .select(List.of(new ValueWrapper("main").as("schemaname")))
+                .from(pragma)
+                .select(pragma.field("name").as("schemaname"))
                 .getSql();
     }
 
     @Override
     public String listTables() {
-        return listTablesOfSchema("main");
+        Selectable table_list = new TableValuedFunction("pragma_table_list").as("table_list");
+        return new SQLiteQuery()
+                .from(table_list)
+                .select(
+                        table_list.field("schema").as("schemaname"),
+                        table_list.field("name").as("tablename")
+                )
+                .getSql();
     }
 
     @Override
