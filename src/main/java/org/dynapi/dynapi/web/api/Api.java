7c7c80d7308eaeb1c341ae4b322a680133ec6902
playerg9 <g9yout@gmail.com>
2024-08-25 14:39:58 +0200

    added methods for the api endpoints

--- src/main/java/org/dynapi/dynapi/web/api/Api.java
+++ src/main/java/org/dynapi/dynapi/web/api/Api.java
@@ -1,9 +1,11 @@
 package org.dynapi.dynapi.web.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * query endpoint for the data
@@ -13,5 +15,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class Api {
+    /**
+     * fetches a list of entries
+     */
+    @GetMapping(value = "/{schema}/{table}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getAll(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
 
+    /**
+     * fetches exactly one entry or throws `404 Not Found`
+     */
+    @GetMapping(value = "/{schema}/{table}/one", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getOne(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * adds one or more entries
+     */
+    @PostMapping(value = "/{schema}/{table}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> post(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * replaces all matching with the passed entry
+     */
+    @PutMapping(value = "/{schema}/{table}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> put(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * partially modifies a resource
+     */
+    @PatchMapping(value = "/{schema}/{table}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> patch(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * deletes one or more entries
+     */
+    @DeleteMapping(value = "/{schema}/{table}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> delete(HttpServletRequest request, @PathVariable String schema, @PathVariable String table) {
+        return ResponseEntity.ok().build();
+    }
 }
