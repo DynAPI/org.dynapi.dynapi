c29f141c66cc6a37f24547d3092fd7f2f7b0e3ac
playerg9 <g9yout@gmail.com>
2024-09-03 12:43:49 +0200

    added missing annotations

--- src/main/java/org/dynapi/dynapi/web/api/get/GetOpenApiProvider.java
+++ src/main/java/org/dynapi/dynapi/web/api/get/GetOpenApiProvider.java
@@ -1,13 +1,16 @@
 package org.dynapi.dynapi.web.api.get;
 
 import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dynapi.dynapi.core.openapi.OpenApiManager;
 import org.dynapi.dynapi.core.openapi.OpenApiProvider;
 import org.dynapi.openapispec.OpenApiSpecBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@AllArgsConstructor
 @Component
 public class GetOpenApiProvider implements OpenApiProvider {
     @PostConstruct
