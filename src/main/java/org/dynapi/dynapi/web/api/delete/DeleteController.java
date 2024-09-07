ead237c36248c681f241dbd144100154b260d350
playerg9 <g9yout@gmail.com>
2024-09-07 14:15:09 +0200

    implemented `DELETE /{schema}/{table}/{rowid}`

--- src/main/java/org/dynapi/dynapi/web/api/delete/DeleteController.java
+++ src/main/java/org/dynapi/dynapi/web/api/delete/DeleteController.java
@@ -3,8 +3,14 @@ package org.dynapi.dynapi.web.api.delete;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dynapi.dynapi.core.database.interfaces.Database;
+import org.dynapi.squirtle.core.PseudoColumns;
+import org.dynapi.squirtle.core.queries.QueryBuilder;
+import org.dynapi.squirtle.core.queries.Schema;
+import org.dynapi.squirtle.core.queries.Table;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class DeleteController {
+    private final Database database;
+    private final JdbcTemplate jdbcTemplate;
+
     /**
      * deletes one or more entries
      */
@@ -32,11 +41,22 @@ public class DeleteController {
      */
     @DeleteMapping(value = "/{schema}/{table}/{rowid}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteByRowId(
-            HttpServletRequest request,
             @PathVariable("schema") String schemaName,
             @PathVariable("table") String tableName,
             @PathVariable("rowid") long rowid
     ) {
-        return ResponseEntity.ok().build();
+        Schema schema = new Schema(schemaName);
+        Table table = schema.table(tableName);
+        QueryBuilder queryBuilder = database.newQuery()
+                .from(table)
+                .delete()
+                .where(PseudoColumns.RowID.eq(rowid))
+                .limit(1);
+
+        String sqlQuery = queryBuilder.getSql();
+        log.info(sqlQuery);
+
+        jdbcTemplate.execute(sqlQuery);
+        return ResponseEntity.noContent().build();
     }
 }
